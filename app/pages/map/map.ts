1c45490ae22e0b983455b345abb2fd31be9e8666
gavs9 <gavs272@gmail.com>
2016-09-25 14:56:03 -0600

    Mapa simple... Trabajando en agregar mas funciones

--- app/pages/map/map.ts
+++ app/pages/map/map.ts
@@ -1,23 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef} from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Geolocation} from 'ionic-native';
 import { SettingsPage} from '../settings/settings';
 
-/*
-  Generated class for the MapPage page.
+// Instalar esto para que funcione gps: ionic plugin add cordova-plugin-geolocation
+// Geolocation es asincrono
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+declare var google;
+
 @Component({
   templateUrl: 'build/pages/map/map.html',
 })
 export class MapPage {
 
   public settingsPage : any;
-  constructor(private navCtrl: NavController) {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
+  constructor(public navCtrl: NavController) {
     this.settingsPage = SettingsPage;
+  }
+
+  ionViewLoaded(){
+    this.loadMap();
+  }
+
+  loadMap(){  // Escuela Informatica 9.970650, -84.129229
+ 
+    Geolocation.getCurrentPosition().then((position) => {
+ 
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+ 
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+      return position;
+ 
+    }, (err) => {
+      console.log(err);
+      
+    }).then((position) => {
+        this.addMarker({lat: position.coords.latitude, lng: position.coords.longitude});
+    });
+ 
+  }
+
+  addMarker(...args){
+ 
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: (args.length == 0) ? this.map.getCenter() : args[0]
+    });
   
+    let content = "<h4>Hola, somos Tico Rails!</h4>";          
+  
+    this.addPopUpMessage(marker, content);
+ 
+  }
+
+  addPopUpMessage(marker, content){
+  
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+  
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+ 
+  }
 }
 
-}
