e53ebc36ee1d89a1e9b3a395a05ba10ae35ece59
gavs9 <gavs272@gmail.com>
2016-10-22 23:46:46 -0600

    share option added * news page

--- app/pages/news/news.ts
+++ app/pages/news/news.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
 import { SettingsPage} from '../settings/settings';
 import { Http } from '@angular/http';
+import { Toast, InAppBrowser, SocialSharing } from 'ionic-native';
 import 'rxjs/add/operator/map';
 
 /*
   News section for TicoRails
 */
+declare var window: any;
+
 @Component({
   templateUrl: 'build/pages/news/news.html',
 })
@@ -32,14 +35,25 @@ export class NewsPage {
   }
 
   launch(url) {
-        var cordova;
         if (this.platform.is('cordova')) {
             this.platform.ready().then(() => {
-              cordova.InAppBrowser.open(url, "_system", "location=true");
+              window.plugins.toast.show("READY", "short", "center");
+              InAppBrowser.open(url, "_system", "location=true");
             });
         }else{
             window.open(url,'_blank');
         }
+    }
+
+    whatsappShare(url){
+
+        SocialSharing.shareViaWhatsApp("Enviado por TicoRails", null /*Image*/,  url /* url */)
+        .then(()=>{
+            console.log("Success");
+        },
+        ()=>{
+            console.log("failed")
+        })
     }
 
 }
