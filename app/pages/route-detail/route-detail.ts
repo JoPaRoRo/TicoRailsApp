5cd3045aad030a7aafa4523b377aaecb3d894bdb
JoPaRoRo <jose.rdrz.rojas@gmail.com>
2016-10-25 20:58:52 -0600

    New Route pages created

--- app/pages/route-detail/route-detail.ts
+++ app/pages/route-detail/route-detail.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 /*
-  Generated class for the RoutePage page.
+  Generated class for the RouteDetailPage page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
 @Component({
-  templateUrl: 'build/pages/route/route.html',
+  templateUrl: 'build/pages/route-detail/route-detail.html',
 })
-export class RoutePage {
+export class RouteDetailPage {
 
   constructor(private navCtrl: NavController) {
 
