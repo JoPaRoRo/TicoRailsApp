585ef49a6c3066d31641c30cde7b30e352c7b720
JoPaRoRo <jose.rdrz.rojas@gmail.com>
2016-10-28 20:06:46 -0600

    Moda Map Added

---
+++ app/pages/modal-map/modal-map.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the ModalMapPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/modal-map/modal-map.html',
+})
+export class ModalMapPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
