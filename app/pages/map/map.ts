e4d195cdfb7be8cc8cea8e459260ea686a7e1685
gavs9 <gavs272@gmail.com>
2016-11-01 02:06:35 -0600

    Map (searchbar, routes)

--- app/pages/map/map.ts
+++ app/pages/map/map.ts
@@ -1,7 +1,9 @@
 import { Component, ViewChild, ElementRef} from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from 'ionic-native';
+import { Http } from '@angular/http';
 import { SettingsPage } from '../settings/settings';
+import 'rxjs/add/operator/map';
 
 // Instalar esto para que funcione gps: ionic plugin add cordova-plugin-geolocation
 // Geolocation es asincrono
@@ -17,16 +19,81 @@ export class MapPage {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
   searchMap; // Buscar estacion
+  stations: any;
 
-  items = ["Est1", "Est2", "Est3"];
-
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, public http: Http) {
+    //this.loadMarkers();
     this.settingsPage = SettingsPage;
   }
 
   ionViewLoaded(){
     this.loadMap();
+  }
+
+  loadMarkers(){
+    //Loading news from TicoRails API
+    /*this.http.get('https://ticorailsapi.herokuapp.com/api/station').map(res => res.json()).subscribe(
+        data => {
+            this.stations = data;
+        },
+        err => {
+            console.log("Error reading TicoRails Stations!");
+        }
+    );*/
+
+    if (this.stations) {
+      return Promise.resolve(this.stations);
+    }
+
+    return new Promise(resolve => {
+      this.http.get('https://ticorailsapi.herokuapp.com/api/station')
+      .map(res => res.json())
+        .subscribe(data => {
+          this.stations = data;
+          resolve(this.stations);
+        });
+    });
+
+  }
+
+  addPopUpMessage(marker, content){
+  
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+  
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+ 
+  }
+
+  routeMarker(imgUrl, pos, cont){
+
+    var image = imgUrl;
+ 
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: pos,
+      icon: image
+    });
+  
+    let content = cont;          
+  
+    this.addPopUpMessage(marker, content);
+ 
+  }
+
+  drawingStations(arr){
+      this.stations.forEach(function(element){
+          if(element.lat != null && element.lng != null){
+            //let obj = new google.maps.LatLng(element.lat, element.lng);
+            console.log(element.lat +" , " + element.lng);
+            //this.routeMarker("assets/img/station.png", {lat: 1, lng: 1}, "Hola");
+            
+          }
+      });
   }
 
 
@@ -52,6 +119,10 @@ export class MapPage {
       
     }).then((position) => {
         this.addMarker({lat: position.coords.latitude, lng: position.coords.longitude});
+        return this.loadMarkers();
+    }).then((res) => {
+        //console.log(this.stations[0].station);
+        this.drawingStations(res);
     });
  
   }
@@ -74,41 +145,19 @@ export class MapPage {
  
   }
 
-  routeMarker(imgUrl, pos, cont){
-
-    var image = imgUrl;
- 
-    let marker = new google.maps.Marker({
-      map: this.map,
-      animation: google.maps.Animation.DROP,
-      position: pos,
-      icon: image
-    });
-  
-    let content = cont;          
-  
-    this.addPopUpMessage(marker, content);
- 
-  }
-
-  addPopUpMessage(marker, content){
-  
-    let infoWindow = new google.maps.InfoWindow({
-      content: content
-    });
-  
-    google.maps.event.addListener(marker, 'click', () => {
-      infoWindow.open(this.map, marker);
-    });
- 
-  }
-
   searchLocation(itemSelected)
   {
-    console.log("Buscar " + itemSelected);
+  
+    let myStation = this.stations.filter(function(element){
+                                          return element.station == itemSelected;
+    });
+
+     console.log("Buscar " + myStation[0].station);
+     this.takeMeTo({lat: myStation[0].lat, lng: myStation[0].lng});
+
   }
 
-  nearestLocation(){
+  takeMeTo(toPos){
     console.log("Nearest");
 
     Geolocation.getCurrentPosition().then((position) => {
@@ -132,15 +181,18 @@ export class MapPage {
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
       directionsDisplay.setMap(this.map);
 
-      this.calculateAndDisplayRoute(directionsService, directionsDisplay, position);
+      var pointA = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      var pointB = new google.maps.LatLng(toPos.lat, toPos.lng);
+
+      this.calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB);
       });
     
   }
 
-  calculateAndDisplayRoute(directionsService, directionsDisplay, myPos) {
+  calculateAndDisplayRoute(directionsService, directionsDisplay, fromPos, toPos) {
 
-    var pointA = new google.maps.LatLng(myPos.coords.latitude, myPos.coords.longitude);
-    var pointB = new google.maps.LatLng(9.970650, -84.129229);
+    var pointA = fromPos;
+    var pointB = toPos;
 
     this.routeMarker("assets/img/myLocation.png", pointA, "<h4>Usted está aquí!</h4>");
     this.routeMarker("assets/img/arrived.png", pointB, "<h4>Este es su destino!</h4>");
